374511c3615d8e4de02c467d9affc4adecea5675
Teresa Nededog <tnededog@my.hpu.edu>
2015-01-28 11:29:55 -1000

    Added validation to function string inputs

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 module.exports = {
 
   findMissingNumbers : function (sequence) {
+    
+    if (typeof sequence !== 'string') {
+      return 'ERROR: arguement should be a string';
+    }
+
     var possibleDigits = '0123456789';
     
-    var missingDigits = '';
+    var missingDigits = [];
 
     for (var i = 0; i < possibleDigits.length; i++) {
       if (sequence.indexOf(possibleDigits[i]) === -1) {
-        missingDigits = missingDigits.concat(possibleDigits[i]);
+        missingDigits.push(parseInt(possibleDigits[i], 10));
       }
     }
     
@@ -17,12 +22,20 @@ module.exports = {
 
   swapWords : function (stringArg) {
 
+    if (typeof stringArg !== 'string') {
+      return 'ERROR: arguement should be a string';
+    }
+
     var regEx = /(\w+)\s+(\w+)/;
     return stringArg.replace(regEx, "$2, $1");
   
   },
 
   twoStrings : function (locateStr, circleStr) {
+
+    if (typeof locateStr !== 'string' || typeof circleStr !== 'string' ) {
+      return 'ERROR: arguements should be a strings';
+    }
     var dblCircleStr = circleStr.concat(circleStr);
 
     return dblCircleStr.indexOf(locateStr) >= 0;
@@ -31,7 +44,12 @@ module.exports = {
 
   palindrome : function (word) {
 
+    if (typeof word !== 'string') {
+      return 'ERROR: arguement should be a string';
+    }
+
     return word === word.split('').reverse().join('');
 
   }
+  
 };
