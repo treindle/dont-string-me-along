498fdae91ce690738e93fa18607b172501f4fbb4
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-27 21:58:38 -1000

    findMissingNumbers implementation

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 module.exports = {
 
   findMissingNumbers : function () {
+    var possibleDigits = '0123456789';
+    
+    var missingDigits = '';
 
-var sequence = "9899100101103104105";
+    var sequence = "9899100101103104105";
+
+    for (var i = 0; i < possibleDigits.length; i++) {
+      if (sequence.indexOf(possibleDigits[i]) === -1) {
+        missingDigits = missingDigits.concat(possibleDigits[i]);
+      }
+    }
+    
+    return missingDigits;
 
   },
 
   swapWords : function () {
-var name = "Jay Won";
+    var name = "Jay Won";
 
   },
 
